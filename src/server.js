2c94919616c9c6c74ac0ff0f722e828dae867949
AmaanIshrat <ishratamaan@gmail.com>
2025-02-10 11:49:45 +0530

    new

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './src/routes/authRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config(); // Load environment variables from .env
 

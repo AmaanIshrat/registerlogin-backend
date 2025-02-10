46baeaa089016b3ab0ce9cee0b9e2145a9dc0116
AmaanIshrat <ishratamaan@gmail.com>
2025-02-10 10:43:22 +0530

    new-updates

--- server.js
+++ server.js
@@ -9,13 +9,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, your backend is working!");
+});
+
+// Set PORT for Vercel
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
 app.use('/api/auth', authRoutes);
 
-mongoose.connect('mongodb://localhost:27017/userdb', {
+mongoose.connect('mongodb+srv://Amaan:Ishrat123@cluster0.4pjjs.mongodb.net/firstapp?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => {
     app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+    console.log("connected to db...")
   })
   .catch((err) => console.log(err));
